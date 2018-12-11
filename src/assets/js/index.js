864b839e72a5151683d3f5065f63066259169922
chrism6475 <chrism6475@gwu.edu>
2018-12-10 20:24:45 -0600

    add sample indicators

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -18,15 +18,14 @@ var dataSetSearch = new Vue({
                           url: "here"},
               ],
       indicatorOption: '',
-      countries: [ "AL","AK", "AS","AZ","AR","CA", "CO","CT", "DE","DC", "FM","FL", "GA", "GU", "HI",
-        "ID","IL","IN","IA","KS", "KY","LA", "ME","MH", "MD","MA","MI","MN","MS", "MO", "MT",
-        "NE","NV","NH","NJ","NM","NY","NC","ND","MP","OH","OK","OR","PW","PA", "PR","RI",
-        "SC","SD", "TN","TX", "UT","VT","VI", "VA","WA", "WV","WI", "WY"],
+      countries: [],
       indicators: []
   },
   mounted() {
+      console.log("here");
       axios.get('./assets/json/countries.json').then(response => this.countries = response);
       console.log("here");
+      axios.get('./assets/json/indicators.json').then(response => this.indicators = response);
 
   },
 
