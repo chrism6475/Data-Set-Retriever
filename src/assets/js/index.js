84b639b2dc139278d2cdc45f26b9bc5e2a664160
chrism6475 <chrism6475@gwu.edu>
2018-12-10 19:52:43 -0600

    Create dummy result data, establish one-way binding

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,18 +1,29 @@
 const BASE_URL = 'http://api.worldbank.org/v2/countries/all/indicators/SP.POP.TOTL?date=2000:2001';
 
-var searchArtistRequest = new Vue({
+var dataSetSearch = new Vue({
   el: '#searchCriteriaForm',
   data:{
       noResults: false,
       searchText: '',
       countryOption: '',
+      yearOption: '',
       test: "test",
-      results:[],
+      results:[{ name: "First Data Set",
+                year: 1969,
+                indicator: "GDP",
+                url: "here"},
+                {
+                  name: "Second Data Set",
+                          year: 1969,
+                          indicator: "GDP",
+                          url: "here"},
+              ],
       indicatorOption: '',
       countries: [ "AL","AK", "AS","AZ","AR","CA", "CO","CT", "DE","DC", "FM","FL", "GA", "GU", "HI",
         "ID","IL","IN","IA","KS", "KY","LA", "ME","MH", "MD","MA","MI","MN","MS", "MO", "MT",
         "NE","NV","NH","NJ","NM","NY","NC","ND","MP","OH","OK","OR","PW","PA", "PR","RI",
-        "SC","SD", "TN","TX", "UT","VT","VI", "VA","WA", "WV","WI", "WY"]
+        "SC","SD", "TN","TX", "UT","VT","VI", "VA","WA", "WV","WI", "WY"],
+      indicators: []
   },
   computed: {
     searchForArtist: function(){
